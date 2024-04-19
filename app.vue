0d427eaf1758d66bc37c407b3292971f4627bdf9
MinatoNami <minato.nami.dev@gmail.com>
2024-04-19 10:20:29 +0800

    Add layouts

--- app.vue
+++ app.vue
@@ -8,11 +8,8 @@ useHead({
 
 <template>
   <div>
-    <nav>
-      <NuxtLink href="/">Home</NuxtLink>
-      <NuxtLink href="/movies">Movies</NuxtLink>
-      <NuxtLink href="https://vueschool.io">Vue School</NuxtLink>
-    </nav>
-    <NuxtPage />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav style="display: flex; gap: 10px">
+    <NuxtLink href="/">Home</NuxtLink>
+    <NuxtLink href="/movies">Movies</NuxtLink>
+    <NuxtLink href="https://vueschool.io">Vue School</NuxtLink>
+  </nav>
+  <slot></slot>
+</template>
